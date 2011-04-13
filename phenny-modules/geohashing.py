e557b62ef595b3b8097da374c7069a3d11baf350
Thomas Hirsch <fwd.github.20.ravn@neverbox.com>
2011-04-13 14:53:38 +0200

    update from production: .where's [user], use crox data source, rm registration procedures, watch spam, watch wiki up/down status

--- phenny-modules/geohashing.py
+++ phenny-modules/geohashing.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import re, web, datetime, hashlib, struct, yaml
+import re, web, datetime, hashlib, struct, yaml, random
 from GraticuleDatabase import GraticuleDatabase
 import xml.etree.ElementTree as ET
 import geohash as geohashorg
 
-lazy = "../code/pywikipedia/lastchangeset.yaml"
+lazy = "../pywikipedia/lastchangeset.sqlite"
 db = GraticuleDatabase(lazy)
+
+channel = "#geohashing"
 
 def setup(self):
   try: 
@@ -16,37 +18,37 @@ def setup(self):
   except:
     self.geohashing = {}
 
-def register(phenny, input):
-  nick = input.nick
-  data = phenny.geohashing.get(nick,[])
-  words = input.split()
-  reg  = (words[0] == '.register')
-  lat, lon, place = identifyPlace(" ".join(words[1:]))
-  if not lat:
-    phenny.say("I don't know where that is.")
-    return
-  if reg:
-    if (lat,lon) in data:
-      phenny.say("You're already registered to receive updates for graticule %s,%s (%s)." % (lat,lon,place))
-      return
-    else:
-      data.append((lat,lon))
-      phenny.geohashing[nick] = data
-      phenny.say("Ok. You will now receive updates for graticule %s,%s (%s)." % (lat,lon,place))
-  else:
-    if not (lat,lon) in data:
-      phenny.say("You aren't currently registered to receive updates for graticule %s,%s (%s)." % (lat,lon,place))
-      return
-    else:
-      data.remove((lat,lon))
-      phenny.geohashing[nick] = data
-      phenny.say("Ok. You will no longer receive updates for graticule %s,%s (%s)." % (lat,lon,place))
-  f = open("geohashing.yaml","w")
-  f.write(yaml.dump(phenny.geohashing))
-  f.close()
-register.commands = ['register', 'unregister']
-register.priority = 'low'
-register.thread = False
+#def register(phenny, input):
+#  nick = input.nick
+#  data = phenny.geohashing.get(nick,[])
+#  words = input.split()
+#  reg  = (words[0] == '.register')
+#  lat, lon, place = identifyPlace(" ".join(words[1:]))
+#  if not lat:
+#    phenny.say("I don't know where that is.")
+#    return
+#  if reg:
+#    if (lat,lon) in data:
+#      phenny.say("You're already registered to receive updates for graticule %s,%s (%s)." % (lat,lon,place))
+#      return
+#    else:
+#      data.append((lat,lon))
+#      phenny.geohashing[nick] = data
+#      phenny.say("Ok. You will now receive updates for graticule %s,%s (%s)." % (lat,lon,place))
+#  else:
+#    if not (lat,lon) in data:
+#      phenny.say("You aren't currently registered to receive updates for graticule %s,%s (%s)." % (lat,lon,place))
+#      return
+#    else:
+#      data.remove((lat,lon))
+#      phenny.geohashing[nick] = data
+#      phenny.say("Ok. You will no longer receive updates for graticule %s,%s (%s)." % (lat,lon,place))
+#  f = open("geohashing.yaml","w")
+#  f.write(yaml.dump(phenny.geohashing))
+#  f.close()
+#register.commands = ['register', 'unregister']
+#register.priority = 'low'
+#register.thread = False
 
 def gratrefresh(phenny, input):
   global db
@@ -63,17 +65,22 @@ def geohash(djia, date):
   sum = hashlib.md5("%s-%s" % (date, djia)).digest()
   lat, lon = [x/2.**64 for x in struct.unpack_from(">QQ", sum)];
   return lat, lon
-  
+
+croxes = ['http://geo.crox.net/djia/%s',
+          'http://www1.geo.crox.net/djia/%s',
+          'http://www2.geo.crox.net/djia/%s']  
 def getdjia(date):
-  try:
-    return web.get("http://geo.crox.net/djia/%s" % date)
-  except:
+  for crox in croxes:
     try:
-      y,m,d = str(date).split("-")
-      return web.get("http://irc.peeron.com/xkcd/map/data/%s/%s/%s" % (y,m,d))
-    except Exception,e:
-      print date, e
-      return None
+      return web.get(crox % date)
+    except:
+      pass
+  try:
+    y,m,d = str(date).split("-")
+    return web.get("http://irc.peeron.com/xkcd/map/data/%s/%s/%s" % (y,m,d))
+  except Exception,e:
+    print date, e
+    return None
 
 def geoSearch(term):
   try:
@@ -90,12 +97,34 @@ def geoSearch(term):
     lon = item.find("lng")
     return (name != None) and (name.text != None) and name.text, (cname != None) and (cname.text != None) and cname.text, (lat != None) and (lat.text != None) and lat.text, (lon != None) and (lon.text != None) and lon.text #sorry.
 
+def findUser(name):
+  if name.lower()=="waldo" or name.lower()=="carmen sandiego":
+    return [str(random.randint(-179,179))+","+str(random.randint(-89,89))]
+  try: 
+    data = web.get("http://wiki.xkcd.com/wgh/api.php?action=query&titles=%s&prop=categories&format=xml&cllimit=500" % ("User:"+name))
+    results=[]
+    if "missing" in data:
+      return None
+    else:
+      tree  = ET.fromstring(data)
+      cats = tree.find("query").find("pages").find("page").find("categories")
+      for cat in cats:
+        tag = cat.get("title")
+        if "Geohashers in" in tag:
+          loc = tag[23:]
+          if re_NUMERIC.match(loc):
+            results.append(loc)
+    return results
+  except Exception,ex:
+    print ex, name
+    return None
+
 def gratinfo(phenny, input):
   term = input
   try: 
-    term = input[input.index(" ")+1:]
+    term = input[input.index(" ")+1:].strip()
   except:
-    phenny.say(".where's (lat,lon|query)")
+    phenny.say(".where's (lat,lon|query|username)")
     return
   found = re_NUMERIC.search(input)
   if found:
@@ -107,19 +136,28 @@ def gratinfo(phenny, input):
         phenny.say("That's %s! http://geohashing.org/%s,%s" % (info[0], lat, lon))
   else:
       grats = db.findAll(term)
+      user  = findUser(term)
       if grats:
         for grat in grats[:5]:
-          phenny.say("There's %s at %s,%s. http://geohashing.org/%s,%s" % (grat[1][0], grat[0][0], grat[0][1], grat[0][0], grat[0][1]))
+          phenny.say("There's %s at %s,%s. http://geohashing.org/%s,%s" % (grat[2], grat[0], grat[1], grat[0], grat[1]))
         if len(grats)>4:
           phenny.say("There's even more, but you have to be more specific.")
-      else:
-        phenny.say("Couldn't find a graticule of that name.")
+      if user:
+        for loc in user:
+          lat, lon = loc.split(",")
+          lookup = db.getLatLon(lat, lon, False)
+          if not lookup:
+            lookup = "Unnamed"
+          else:
+            lookup=lookup[0]
+          phenny.say("That user is%slisted in graticule %s (%s)." % ((loc == user[0]) and " " or " also ", loc, lookup))
+      if (not grats) and (not user):
+        phenny.say("Couldn't find a graticule or user of that name.")
         geo = geoSearch(term)
-        print geo
         if geo and geo[0]:
           phenny.say("Did you mean %s at %s,%s?" % (geo[0]+(geo[1] and ", "+geo[1] or ""), geo[2], geo[3]))
   
-gratinfo.commands = ['grat', 'where\'s', 'graticule']
+gratinfo.commands = ['grat', 'where\'s', 'graticule', 'whereis', 'wheres']
 gratinfo.priority = 'low'
 gratinfo.thread = True
 
@@ -137,7 +175,7 @@ def geolookup(phenny, input, renick=None):
     if hasattr(input, 'sender'):
       renick = input.sender
     else:
-      renick = '#geohashing'
+      renick = channel
   found = re_NUMERICF.search(input)
   if found:
     lat, lon = found.groups(0)
@@ -177,7 +215,7 @@ def geolookup(phenny, input, renick=None):
     phenny.msg(renick, ".lookup lat,lon")
 geolookup.commands = ['lookup']
 geolookup.priority = 'low'
-geolookup.thread = True
+geolookup.thread = True 
   
 def identifyPlace(string):
   lat, lon, place = None, None, None
@@ -189,8 +227,8 @@ def identifyPlace(string):
     else: 
       namematch = db.findAll(string)
       if namematch:
-        lat, lon = namematch[0][0][0], namematch[0][0][1]
-        place = namematch[0][1][0]
+        lat, lon = namematch[0][0], namematch[0][1]
+        place = namematch[0][2]
       else: 
         geo = geoSearch(string)
         if geo:
@@ -209,7 +247,7 @@ def hashes(phenny, input, renick=None):
     if hasattr(input, "sender"):
       renick = input.sender
     else:
-      renick = "#geohashing"
+      renick = channel
   date  = datetime.date.today()
   try:
     if parts[1]=="help":
@@ -255,12 +293,12 @@ def zbotextra(phenny, input):
   if match:
     date = match.groups(0)[0]
     hashes(phenny, ".# global %s" % date, input.sender)
-    if input.nick == 'zbot':
-      for user, grats in phenny.geohashing.items():
-        for grat in grats:
-          lat, lon = grat
-          hashes(phenny, ".# %s,%s %s" % (lat,lon,date), user)
+    #if input.nick == 'zbot':
+    #  for user, grats in phenny.geohashing.items():
+    #    for grat in grats:
+    #      lat, lon = grat
+    #      hashes(phenny, ".# %s,%s %s" % (lat,lon,date), user)
     
-zbotextra.thread = False
+zbotextra.thread = False 
 zbotextra.priority = 'low'
 zbotextra.rule = r'^(W30|All):.*?(\d{4}-\d{2}-\d{2})'
--- phenny-modules/rcwatch.py
+++ phenny-modules/rcwatch.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import yaml, re, time, web
+import yaml, re, time, web, sys
+import urllib
 import xml.etree.ElementTree as ET
+import random
+
+channel = "#geohashing"
+sleepytime = 60
 
 try:
   interesting = yaml.load(open("interesting.yaml", "r").read())
@@ -10,13 +15,20 @@ except:
   interesting = [
     ("New page", re.compile("New page"))
   ]
+try:
+  re_spam = yaml.load(open("spam.yaml", "r").read())
+except:
+  re_spam = []
+try:
+  lyrics = yaml.load(open("lyrics.yaml", "r").read())
+except:
+  lyrics = []
 
 re_noenctags  = re.compile("&lt;.*?&gt;")
 re_notags = re.compile("<.*?>")
 re_noenc  = re.compile("&.*?;")
+re_anonymous = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
 
-def setup(self):
-  self.rcwatch={}
 
 def rexlist(phenny, input): 
    for id, entry in enumerate(interesting):
@@ -25,13 +37,20 @@ rexlist.commands = ['watchlist']
 rexlist.priority = 'low'
 rexlist.thread   = True
 
+def spamlist(phenny, input): 
+   for id, entry in enumerate(re_spam):
+     phenny.say("#%i: pattern '%s'" % (id, entry.pattern))
+spamlist.commands = ['spamlist']
+spamlist.priority = 'low'
+spamlist.thread   = True
+
 def rexadd(phenny, input): 
   if input.sender.startswith('#'): #this command only works in public
     try:
       line = input[input.index(" ")+1:]
       tag  = line[:line.index("=")]
       res  = line[line.index("=")+1:]
-      rec   = re.compile(res, re.IGNORECASE)
+      rec   = re.compile(res, re.IGNORECASE | re.DOTALL)
       interesting.append((tag,rec))
       open("interesting.yaml","w").write(yaml.dump(interesting))
       phenny.say("Notification installed. tag '%s' = pattern '%s'" % (tag, rec.pattern))
@@ -42,6 +61,55 @@ def rexadd(phenny, input):
 rexadd.commands = ['addwatch']
 rexadd.example = ".addwatch badgers=[Bb]adger"
 rexadd.priority = 'low'
+
+def spamadd(phenny, input): 
+  if input.sender.startswith('#') or input.sender == "relet": #this command only works in public
+    try:
+      line = input[input.index(" ")+1:]
+      res  = line.strip()
+      rec   = re.compile(res, re.IGNORECASE)
+      re_spam.append(rec)
+      open("spam.yaml","w").write(yaml.dump(re_spam))
+      phenny.say("Spam trigger installed. pattern '%s'" % (rec.pattern))
+    except Exception, ex:
+      phenny.say("Could not add this. %s" % str(ex))
+  else:
+    phenny.say("Please do this in a public channel.")
+spamadd.commands = ['addspam']
+spamadd.example = ".addspam growth hormones"
+spamadd.priority = 'low'
+
+def testspam(phenny, input):
+  try:
+    for re in re_spam:
+      if re.search(input):
+        phenny.say("%s matches." % re.pattern)
+  except Exception,ex:
+    phenny.say("There is a fail: %s" % str(ex))
+testspam.commands=["testspam"]
+testspam.priority="low"
+
+lyricsenders = []
+def addlyrics(phenny, input):
+  global lyrics
+  if input.sender.startswith("#"):
+    return
+  if input==".endlyrics":
+    try:
+      lyricsenders.remove(input.nick)
+      phenny.say("Thank you!")
+    except:
+      phenny.say("Hmm. Are you sure you used the .lyrics command first?")
+    return
+  if input[:7]==".lyrics":
+    lyricsenders.append(input.nick)
+    phenny.say("Ok. Go ahead. Send me a .endlyrics when you're done, or I'm going to publish everything you say on the Internet. I mean it.")
+    return
+  if input.nick in lyricsenders:
+    lyrics.append(input.strip())
+    open("lyrics.yaml","w").write(yaml.dump(lyrics))
+addlyrics.rule = r'.*'
+addlyrics.priority = 'low'
 
 def rexremove(phenny, input): 
   if input.sender.startswith('#'): #this command only works in public
@@ -58,13 +126,30 @@ def rexremove(phenny, input):
 rexremove.commands = ['rmwatch']
 rexremove.priority = 'low'
 
+def spamremove(phenny, input): 
+  if input.sender.startswith('#') or input.sender=="relet": #this command only works in public
+    try:
+      line = input[input.index(" ")+1:]
+      id = int(line)
+      rec = re_spam.pop(id)
+      open("spam.yaml","w").write(yaml.dump(re_spam))
+      phenny.say("Spam trigger %s removed." % (rec.pattern))
+    except Exception, ex:
+      phenny.say("Could not remove this. %s" % str(ex))
+  else:
+    phenny.say("Please do this in a public channel.")
+spamremove.commands = ['rmspam']
+spamremove.priority = 'low'
+
 def rexstart(phenny, input):  
   if "running" in phenny.rcwatch:
-    phenny.say("Watch daemon is running already.") 
+    if ".rcwatch" in input:
+      phenny.say("Watch daemon is running already.") 
     return
   phenny.rcwatch["running"] = True
 
-  phenny.msg("#geohashing", "Ok. Running the watch daemon.")
+  #phenny.msg(channel, "Ok. Running the watch daemon.")
+  annoying = False
 
   oldrc = None
   try:
@@ -73,22 +158,39 @@ def rexstart(phenny, input):
     oldrc = "0"
 
   while True:
-    time.sleep(60)
+    time.sleep(sleepytime)
     
     if "stopme" in phenny.rcwatch:
       phenny.say("Aborted the watch daemon")
       del phenny.rcwatch["stopme"]
       return
 
-    rc = web.get("http://wiki.xkcd.com/wgh/api.php?action=query&format=xml&list=recentchanges&rcprop=user|comment|flags|title|ids")
+    rc = None
+    try:
+      rc = web.get("http://wiki.xkcd.com/wgh/api.php?action=query&format=xml&list=recentchanges&rcprop=user|comment|flags|title|ids&rclimit=20")
+    except:
+      phenny.rcwatch["down"]=phenny.rcwatch.get("down",0)+1
+      if phenny.rcwatch["down"]==3:
+        phenny.msg(channel,"The wiki %s. \o/" % (random.choice(["has gone down.", "is no more.", "is unreachable.", "has disappeared.", "- I can't find it"])))
     if not rc:
       continue
+    if "down" in phenny.rcwatch:
+      if phenny.rcwatch["down"]>3:
+        phenny.msg(channel,"The wiki is back!")
+      del phenny.rcwatch["down"]
     
     et = ET.fromstring(rc)
-    
+    if not et:
+      print "No page returned."
+      return   
+ 
     newestcurrentid = "0"
 
-    rclist = et.find("query").find("recentchanges")
+    try:
+      rclist = et.find("query").find("recentchanges")
+    except Exception,ex:
+      print ex
+      continue 
     hadthese = []
     for entry in rclist:
       rcid = entry.get("rcid")
@@ -101,28 +203,98 @@ def rexstart(phenny, input):
       user = entry.get("user")
       comment = entry.get("comment")
       new = "new" in entry.keys()
+      bot = "bot" in entry.keys()
       minor = "minor" in entry.keys()
       pageid = entry.get("pageid")
+      revid = entry.get("revid")
       
       if pageid in hadthese:
         continue
       hadthese.append(pageid)
 
-      show = None
-      for tag, re in interesting:
-        if re.search(comment) or re.search(title) or re.search(user):
-          show=tag
-      if show!=None and minor==False:
-        phenny.msg("#geohashing","News about \"%s\": http://geohashing.org/%s was edited by %s. (%s)" % (show, title, user, comment))
-      elif new:
-        phenny.msg("#geohashing","New page: http://geohashing.org/%s was created by %s. (%s)" % (title, user, comment))
+      show, spam = None, None
+
+      if re_anonymous.match(user):
+        try:
+          page = web.get("http://wiki.xkcd.com/wgh/index.php?title=%s&action=raw" % urllib.quote(title))
+          for re in re_spam:
+            if re.search(comment) or re.search(title) or re.search(page):
+              spam = True
+        except Exception, ex:
+          print "while fetching page:"
+          print ex #if it doesn't work, don't bother the channel. log it though.
+
+      if "[Shmulik]" in comment:
+        spam=False
+        minor=True #prevent reporting, too
+
+      if spam:
+        try:
+	  #login
+          #result = web.post("http://wiki.xkcd.com/wgh/api.php", {"action":"login", "lgname":"ReletBot", "lgpassword":"...", "format":"xml"})
+          #result might be a fail, most frequently because we are being throttled.
+          #get rollback token
+          rvpage = web.post("http://wiki.xkcd.com/wgh/api.php", {"action":"query", "titles":title, "prop":"info", "intoken":"edit", "format":"xml"})
+          et2 = ET.fromstring(rvpage)
+	  try:
+            info = et2.find("query").find("pages").find("page")
+	    token = info.get("edittoken")
+            rvcheck = info.get("lastrevid")
+            if rvcheck != revid:
+              print "Someone else seems to have reverted this spam. (%s,%s)" % (title, revid) 
+	      continue
+          except Exception,ex:
+            print "while fetching edit token:"
+            print ex
+            continue
+
+          #get a lyric comment
+          comment = "[Shmulik] "
+          if len(lyrics)>0:
+	    annoying = False
+            comment+=lyrics.pop(0)
+            open("lyrics.yaml","w").write(yaml.dump(lyrics))
+          else:
+	    comment+="undoing the last edit, because I think it looks like spam."
+          if len(lyrics)==10:
+            phenny.msg(channel, "I'm running out of spam-undo-lyrics. Please send me some in a private message using the .lyrics command.")
+          if len(lyrics)==0:
+            if not annoying:
+              phenny.msg(channel, "Oh no! I've run out of spam-undo-lyrics. Please do send me some lyrics in a private message using the .lyrics command.")
+              annoying = True
+
+          #undo spam
+	  if typ=="new":
+            result = web.post("http://wiki.xkcd.com/wgh/api.php", {"action":"edit", "title":title, "bot":"0", "text":"{{spam}}", "token":token, "summary":comment, "format":"xml"})
+          else:
+            result = web.post("http://wiki.xkcd.com/wgh/api.php", {"action":"edit", "title":title, "undo":rvcheck, "bot":"0", "token":token, "summary":comment, "format":"xml"})
+
+        except Exception, ex:
+          print "while undoing:"
+          print ex #if it doesn't work, don't bother the channel. log it though.
+        continue #do not announce the page if it is spam!
+      
+      else:
+        for tag, re in interesting:
+          if re.search(comment) or re.search(title) or re.search(user):
+            show=tag
+        if show!=None and minor==False:
+          if "[live]" in comment:
+            phenny.msg(channel,"%s: %s (http://geohashing.org/%s)" % (user, comment, title.replace(' ','_')))
+          else:
+            phenny.msg(channel,"\"%s\": http://geohashing.org/%s was edited by %s. (%s)" % (show, title.replace(' ','_'), user, comment))
+        elif new and (not bot) and (not minor):
+          phenny.msg(channel,"New page: http://geohashing.org/%s was created by %s. (%s)" % (title.replace(' ','_'), user, comment))
 
     if newestcurrentid > oldrc:
       oldrc = newestcurrentid
     open("oldrc.yaml","w").write(yaml.dump(oldrc))
-rexstart.commands = ['rcwatch']
+rexstart.rule = r'.*'
 rexstart.priority = 'low'
-rexstart.thread   = True
+rexstart.thread   = True 
+
+def setup(self):
+  self.rcwatch={}
 
 def stopwatch(phenny, input): 
   phenny.rcwatch["stopme"]=True
